1b4592711f0430920954f7578eb7a3588b30b0dc
Fatima-93 <fatimaalahmad@googlemail.com>
2020-04-30 01:45:41 +0100

    removed traits.php as it was an empty file, and slightly modified wifreframe.hmtl file

--- Library/Traits/Traits.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/* Traits are PHP's way of adding extra functionality to a class.
- * Traits contain re-usable logic.
- * Why do we need a trait - can't we just extend another class?
- * What if there's logic from two classes that you'd like to re-use?
- */
-
